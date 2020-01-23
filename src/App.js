bd3543303948f115a0a00e06b49e90ace34ab8df
SamanthaAO <samanthaaorcutt@gmail.com>
2020-01-23 15:20:27 -0800

    updated layout to use fast

--- src/App.js
+++ src/App.js
@@ -1,175 +1,158 @@
-// TESTING KEY
-
 import React from 'react';
 import dino from './newDino.png';
 import tree from './tree.png';
-// import table from './table.js';
-import { AnimateTo, AnimateSequence, AnimateGroup } from "@microsoft/fast-animation";
-import { Button, ButtonAppearance, Image, Heading, Paragraph, Pivot, TextAction } from "@microsoft/fast-components-react-msft";
-// import {TestGlyph} from "@microsoft/fast-glyphs-msft";
+import animate from './animate.js';
+import { DesignSystemDefaults } from '@microsoft/fast-components-styles-msft';
+import { Button, ButtonAppearance, Image, Heading, Paragraph} from "@microsoft/fast-components-react-msft";
+
+import { DesignSystemProvider, JSSManager  } from "@microsoft/fast-jss-manager-react";
+import { mergeDesignSystem} from "@microsoft/fast-jss-manager";
 import { Page, Grid, Column} from "@microsoft/fast-layouts-react";
 
 import './App.css';
 
+const newDesign = {
+  backgroundColor: "#0F0"
+};
+
+
+
 class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.myDesign = mergeDesignSystem(DesignSystemDefaults, newDesign);
+  }
 
-  animate() {
-    const dinoAnimate = document.querySelector("#dino");
-    const treeAnimate1 = document.querySelector("#tree1");
-    const treeAnimate2 = document.querySelector("#tree2");
-    const treeAnimate3 = document.querySelector("#tree3");
-
-
-    const x = 80;
-    const angle = 22.5;
-    
-    
-
-    const mySequence = new AnimateGroup([
-      // new AnimateTo(dinoAnimate, { x: 250, y: 50, top: "50%", left: "50%" }, { duration: 200 }),
-      new AnimateTo(dinoAnimate, { x: 2000}, { duration: x*20 }),
-      new AnimateTo(treeAnimate1, { rotate: 90}, { delay:x }),
-      //new AnimateTo(dinoAnimate, { x: 300}, { duration: x }),
-      new AnimateTo(treeAnimate2, { rotate: 90}, { delay:x*5 }),
-      //new AnimateTo(dinoAnimate, { x: 500}, { duration: x }),
-      new AnimateTo(treeAnimate3, { rotate: 90}, { delay:x*6 }),
-      //new AnimateTo(dinoAnimate, { x: 1000}, { duration: x }),
-
-      // new AnimateTo(dinoAnimate, { rotate: angle/2 }, { duration: x }),
-      // new AnimateTo(dinoAnimate, { rotate: -angle }, { duration: x, delay:x }),
-      // new AnimateTo(dinoAnimate, { rotate: angle }, { duration: x, delay:x*2 }),
-      // new AnimateTo(dinoAnimate, { rotate: -angle }, { duration: x, delay:x*3 })
-    ]);
-
-    mySequence.onFinish = () => {
-      console.log("Did you like my animations?");
-    }
-
-    
-    mySequence.play();
-
-
-
-    // const fadeOptions= new AnimateConfig ({ opacity: 0 });
-    // new AnimateFrom(myHtmlElement2, fadeOptions, { duration: 100 }).play();
-
-  };
-
-
-
+  
   render() {
+    console.log(DesignSystemDefaults);
+    console.log(newDesign);
+    console.log(this.myDesign);
     return (
-      <div className="App">
-        <Page >
-          <Grid>
-            <Column>
-              <Heading 
-                id="name"
-                tag={"h1"}
-                size={1}
+      <DesignSystemProvider designSystem={this.myDesign}>
+        <div className="App">
+          <Page >
+
+            <Grid>
+
+              <Column 
+              span={12}
+              position={1}
+              gutter={50}
               >
-                Samantha Orcutt
-              </Heading>
-            </Column>
-          </Grid>
-          <Grid>
-            <Column-1 id="profile1">
-              <Image
-                id="profilePicture"
-                src="https://via.placeholder.com/200/09f/fff.png"
-                alt="Samantha Orcutt"
-              />
-            </Column-1>
 
-            <Column-2 id="profile2">
+                <Heading 
+                  tag={"h1"}
+                  size={1}
+                >
+                  Samantha Orcutt
+                </Heading>
 
-              <Heading
-                id="about"
-                tag={"h1"}
-                size={3}
+              </Column>
+
+            </Grid>
+
+            <Grid>
+
+              <Column 
+                id="profile1"
+                span={3}
+                position={1}
               >
-                About Me
-              </Heading>
-              <Paragraph
-                size={1}
+                <Image
+                  id="profilePicture"
+                  src="https://via.placeholder.com/200/09f/fff.png"
+                  alt="Samantha Orcutt"
+                />
+              </Column>
+
+              <Column 
+              span={9}
+              position={4}
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Paragraph>
-            </Column-2>
-          </Grid>
+                
+                <Heading
+                  id="about"
+                  tag={"h1"}
+                  size={3}
+                >
+                  About Me
+                </Heading>
+                <Paragraph
+                  size={1}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Paragraph>
 
-          {/* <TextAction
-        appearance={"filled"}
+              </Column>
 
-        button={
-        <Button>
-          <TestGlyph
-              path={"arrow"}
-          />
-        </Button>}
+            </Grid>
 
-        beforeGlyph={
-        <TestGlyph
-          path={"user"}/>}
-        />  */}
-        {/* <Pivot
-                label={"A set of example text content"}
-                items={table}
-              /> */}
+            <Grid>
 
-          <Grid>
-            <Column id="animateButtonColumn">
-              <Button appearance={ButtonAppearance.primary} onClick={this.animate} >
-                Click me!
-              </Button>
-            </Column>
-          </Grid>
+              <Column id="animateButtonColumn"
+                span={2}
+                position={6}
+              >
+                <Button appearance={ButtonAppearance.primary} onClick={animate} >
+                  Click me!
+                </Button>
+              </Column>
 
-          <Grid id="animationGrid">
+            </Grid>
 
-            <Column-1 id="dinoColumn">
-              <Image
-                id="dino"
-                src={dino}
-                alt="dino"
-              />
-            </Column-1>
+            <Grid id="animationGrid">
 
-            <Column-2 id="treeColumn1">
-              <Image
-                id="tree1"
-                src={tree}
-                alt="tropical tree"
-              />
-            </Column-2>
+              <Column id="dinoColumn"
+                span={4}
+                position={1}
+              >
+                <Image
+                  id="dino"
+                  src={dino}
+                  alt="dino"
+                />
+              </Column>
 
-            <Column-3 id="treeColumn2">
-              <Image
-                id="tree2"
-                src={tree}
-                alt="tropical tree"
-              />
-            </Column-3>
+              <Column
+              span={1}
+              position={9}
+              >
+                <Image
+                  id="tree1"
+                  src={tree}
+                  alt="tropical tree"
+                />
+              </Column>
 
-            <Column-4 id="treeColumn3">
-              <Image
-                id="tree3"
-                src={tree}
-                alt="tropical tree"
-              />
-            </Column-4>
-            
-          </Grid>
+              <Column 
+              span={2}
+              position={10}
+              >
+                <Image
+                  id="tree2"
+                  src={tree}
+                  alt="tropical tree"
+                />
+              </Column>
 
+              <Column 
+                span={1}
+                position={12}
+              >
+                <Image
+                  id="tree3"
+                  src={tree}
+                  alt="tropical tree"
+                />
+              </Column>
+              
+            </Grid>
 
-
-
-
-
-
-        </Page>
-      </div>
+          </Page>
+        </div>
+      </DesignSystemProvider>
 
     );
   }
---
+++ src/testCode.js
@@ -0,0 +1,43 @@
+// import table from './table.js';
+// import {TestGlyph} from "@microsoft/fast-glyphs-msft";
+// import {Pivot, TextAction } from "@microsoft/fast-components-react-msft";
+
+
+
+//********Animate */
+
+// new AnimateTo(dinoAnimate, { x: 250, y: 50, top: "50%", left: "50%" }, { duration: 200 }),
+
+//new AnimateTo(dinoAnimate, { x: 300}, { duration: x }),
+//new AnimateTo(dinoAnimate, { x: 500}, { duration: x }),
+//new AnimateTo(dinoAnimate, { x: 1000}, { duration: x }),
+
+// new AnimateTo(dinoAnimate, { rotate: angle/2 }, { duration: x }),
+// new AnimateTo(dinoAnimate, { rotate: -angle }, { duration: x, delay:x }),
+// new AnimateTo(dinoAnimate, { rotate: angle }, { duration: x, delay:x*2 }),
+// new AnimateTo(dinoAnimate, { rotate: -angle }, { duration: x, delay:x*3 })
+
+
+// const fadeOptions= new AnimateConfig ({ opacity: 0 });
+// new AnimateFrom(myHtmlElement2, fadeOptions, { duration: 100 }).play();
+
+
+
+{/* <TextAction
+          appearance={"filled"}
+
+          button={
+          <Button>
+            <TestGlyph
+                path={"arrow"}
+            />
+          </Button>}
+
+          beforeGlyph={
+          <TestGlyph
+            path={"user"}/>}
+          />  */}
+          {/* <Pivot
+                  label={"A set of example text content"}
+                  items={table}
+                /> */}
