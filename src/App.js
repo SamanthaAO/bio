6de974bb03d20cbe9b178c0e4ff971f65266d709
SamanthaAO <samanthaaorcutt@gmail.com>
2020-01-24 17:43:31 -0800

    added color responses to slider

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { DesignSystemDefaults } from '@microsoft/fast-components-styles-msft';
 import { DesignSystemProvider } from "@microsoft/fast-jss-manager-react";
 import { mergeDesignSystem } from "@microsoft/fast-jss-manager";
 
-import { Slider } from "@microsoft/fast-components-react-msft";
-import {AnimateFrom} from "@microsoft/fast-animation";
+import { Slider, SliderLabel } from "@microsoft/fast-components-react-msft";
+import { AnimateFrom } from "@microsoft/fast-animation";
 
-import { Page} from "@microsoft/fast-layouts-react";
+import { Page, Grid, Column } from "@microsoft/fast-layouts-react";
 
 import './assets/styles/App.css';
 import { info } from "./data/info";
@@ -16,7 +16,7 @@ import { info } from "./data/info";
 import MyHeading from "./components/MyHeading.js";
 import MyInfo from "./components/MyInfo.js";
 import InfoChanger from "./components/InfoChanger.js";
-import {AnimateDino} from "./components/AnimateDino.js";
+import { AnimateDino } from "./components/AnimateDino.js";
 import AnimateDinoButton from "./components/AnimateDinoButton.js";
 
 const newDesign = {
@@ -26,36 +26,84 @@ const newDesign = {
 const myDesign = mergeDesignSystem(DesignSystemDefaults, newDesign);
 
 
+const range = {
+  "minValue": 0,
+  "maxValue": 125
+};
+
 
 class App extends React.Component {
-  
-  constructor (props){
+
+  constructor(props) {
     super(props);
 
     this.increaseInfo = this.increaseInfo.bind(this);
     this.decreaseInfo = this.decreaseInfo.bind(this);
     this.animateInfo = this.animateInfo.bind(this);
+    this.changeColor = this.changeColor.bind(this);
 
     this.state = {
-      info: 0
+      info: 0,
+      backgroundColor: "#FFF"
     }
   }
 
-  animateInfo(){
+  animateInfo() {
     const infoDisplay = document.querySelector("#infoDisplay");
-    const textAnimate =  new AnimateFrom(infoDisplay, { scale: 0}, { duration: 500 });
-
+    const textAnimate = new AnimateFrom(infoDisplay, { scale: 0 }, { duration: 500 });
     textAnimate.play();
   }
 
-  increaseInfo(){
+  increaseInfo() {
     this.animateInfo();
-    this.state.info>=info.sections.length-1 ? this.setState({info:0}) : this.setState({info: this.state.info+1});
+    this.state.info >= info.sections.length - 1 ? this.setState({ info: 0 }) : this.setState({ info: this.state.info + 1 });
   }
 
-  decreaseInfo(){
+  decreaseInfo() {
     this.animateInfo();
-    this.state.info<=0 ? this.setState({info:info.sections.length-1}) : this.setState({info: this.state.info-1});
+    this.state.info <= 0 ? this.setState({ info: info.sections.length - 1 }) : this.setState({ info: this.state.info - 1 });
+  }
+
+  changeColor() {
+    var x = document.querySelector("input").value;
+    //console.log(x);
+    switch(x) {
+        case "1":
+        this.setState({ backgroundColor: "#FFF" })
+        console.log(this.state.backgroundColor);
+        break;
+
+        case "25" :
+        this.setState({ backgroundColor: "#FF0" })
+        console.log(this.state.backgroundColor);
+        break;
+
+        case "50" :
+        this.setState({ backgroundColor: "#050" })
+        console.log(this.state.backgroundColor);
+        break;
+
+        case "75" :
+        this.setState({ backgroundColor: "#900" })
+        console.log(this.state.backgroundColor);
+        break;
+
+        case "100" :
+        this.setState({ backgroundColor: "#005" })
+        console.log(this.state.backgroundColor);
+        break;
+
+        case "125" :
+        this.setState({ backgroundColor: "#000" })
+        console.log(this.state.backgroundColor);
+        break;
+
+        default:
+        console.log(x)
+    }
+
+    
+
   }
 
   render() {
@@ -68,19 +116,56 @@ class App extends React.Component {
 
         <div className="App" style={{ backgroundColor: myDesign.backgroundColor }}>
           <Page >
-            
+
             <MyHeading />
 
-            <Slider />
+            <Grid>
+              <Column
+              position= {1}
+              span={10}
+              >
+            <Slider
+              range={range}
+              id="mySlider"
+              onValueChange={this.changeColor}
+            >
+              <SliderLabel
+                valuePositionBinding={0}
+                label={"white"}
+              />
+              <SliderLabel
+                valuePositionBinding={25}
+                label={"yellow"}
+              />
+              <SliderLabel
+                valuePositionBinding={50}
+                label={"green"}
+              />
+              <SliderLabel
+                valuePositionBinding={75}
+                label={"red"}
+              />
+              <SliderLabel
+                valuePositionBinding={100}
+                label={"blue"}
+              />
+              <SliderLabel
+                valuePositionBinding={125}
+                label={"black"}
+              />
+            </Slider>
+            </Column>
+            </Grid>
 
-            <MyInfo 
-            i = {this.state.info}
+
+            <MyInfo
+              i={this.state.info}
             />
-            
+
             <InfoChanger
-            i = {this.state.info}
-            onClickIncrease = {this.increaseInfo}
-            onClickDecrease = {this.decreaseInfo}
+              i={this.state.info}
+              onClickIncrease={this.increaseInfo}
+              onClickDecrease={this.decreaseInfo}
             />
 
             <AnimateDinoButton />
